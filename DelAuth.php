7314eca2e3aa6d303855ac4e5e5331ed5cb7307e
Kit0ngaMark <139973614+Kit0ngaMark@users.noreply.github.com>
2023-11-24 21:25:43 +0300

    Update DelAuth.php

--- DelAuth.php
+++ DelAuth.php
@@ -13,19 +13,18 @@ if ($conn->connect_error) {
 if ($_SERVER["REQUEST_METHOD"] == "GET" && isset($_GET["id"])) {
     $AuthorID = $_GET["id"];
 
-    // Validate and sanitize input to prevent SQL injection
+   
     $AuthorID = mysqli_real_escape_string($conn, $AuthorID);
 
-    // Check if the author with the given ID exists before attempting to delete
+   
     $checkIfExists = "SELECT * FROM authorstb WHERE AuthorID = '$AuthorID'";
     $result = $conn->query($checkIfExists);
 
     if ($result->num_rows > 0) {
-        // Author exists, proceed with deletion
         $deleteSql = "DELETE FROM authorstb WHERE AuthorID = '$AuthorID'";
 
         if ($conn->query($deleteSql) === true) {
-            // Redirect to viewauthors.php after successful deletion
+        
             header("Location: viewAuthors.php");
             exit();
         } else {
