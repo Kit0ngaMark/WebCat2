d5deaefc2050f6b495318423247a1c1f4d94b459
Kit0ngaMark <139973614+Kit0ngaMark@users.noreply.github.com>
2023-11-24 21:18:02 +0300

    Update constants.php

--- configs/constants.php
+++ configs/constants.php
@@ -1,6 +1,19 @@
 <?php
-    $hostname = "localhost";
-    $username = "root";
-    $userpass = "";
-    $dbname = "authordb";
+
+if (!defined('DB_HOST')) {
+    define('DB_HOST', 'localhost');
+}
+
+if (!defined('DB_USER')) {
+    define('DB_USER', 'root');
+}
+
+if (!defined('DB_PASSWORD')) {
+    define('DB_PASSWORD', '');
+}
+
+if (!defined('DB_NAME')) {
+    define('DB_NAME', 'authordb');
+}
+
 ?>
